4b8b2e00442a8c37c96cfc2d9a344e6f9eb15cb0
projectsdatadna <107699067+projectsdatadna@users.noreply.github.com>
2022-10-10 14:14:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://socketdeploy.herokuapp.com/");
+const socket = io.connect("socketdeploy.herokuapp.com");
 
 function App() {
   const [username, setUsername] = useState("");
